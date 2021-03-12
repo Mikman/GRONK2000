d5b3958262105a239dd76f82e560f95030f9e382
Kasper Grønkjær <kaspergrnkjr@gmail.com>
2021-03-12 14:14:59 +0100

    I2C_attempt_failed

--- Temp, gyro mm opsætning/STM32_MPU6050/Core/Src/main.c
+++ Temp, gyro mm opsætning/STM32_MPU6050/Core/Src/main.c
@@ -40,13 +40,25 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
 
 /* USER CODE BEGIN PV */
+
+static const uint8_t MPU_Address = 0x68 << 1; //Pin AD0 sættes til GND.
+//Da I2C bruger 7 bit addr. og der skal bruges en bit til R/W bit shifter vi 1 til venstre
+
+// MPU registrer til temp
+static const uint8_t MPU_TempRegLSB = 0x41 << 1;
+static const uint8_t MPU_TempRegMSB = 0x42 << 1;
+
+
 
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -63,6 +75,13 @@ void SystemClock_Config(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
+
+	// En enumeration der beskriver en evt. fejl
+	HAL_StatusTypeDef returnValue = HAL_ERROR;
+	uint8_t tempBuf[16];
+	int16_t rawTempVal;
+	float tempVal;
+
 
   /* USER CODE END 1 */
 
@@ -83,6 +102,8 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -91,6 +112,38 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+	  tempBuf[0] = MPU_TempRegMSB;
+	  tempBuf[1] = MPU_TempRegLSB;
+
+	  returnValue = HAL_I2C_Master_Transmit(&hi2c1, MPU_Address, tempBuf, 1, HAL_MAX_DELAY);
+
+	  if (returnValue != HAL_OK){
+		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+		  HAL_Delay(2000);
+		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+	  }else{
+		  returnValue = HAL_I2C_Master_Receive(&hi2c1, MPU_Address, tempBuf, 2, HAL_MAX_DELAY);
+	  }
+
+	  if (returnValue != HAL_OK){
+
+		  for(int i = 0 ; i < 5 ; i = i + 1){
+			  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+			  HAL_Delay(500);
+			  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+			  HAL_Delay(500);
+		  }
+
+	  }else {
+
+		  // sammensætning af rå data
+		  rawTempVal = ((int16_t)tempBuf[0] << 8 | tempBuf[1]);
+		  tempVal = (rawTempVal)/340 + 36.53;
+		  rawTempVal = ((int16_t)tempBuf[0] << 8 | tempBuf[1]);
+	  }
+
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -131,6 +184,66 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */

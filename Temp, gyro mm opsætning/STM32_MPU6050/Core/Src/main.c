20b72145a524b8cc0a81a84b4d7cd234bbd3d693
Kasper Grønkjær <kaspergrnkjr@gmail.com>
2021-03-15 16:26:15 +0100

    LCD display og temp virker

--- Temp, gyro mm opsætning/STM32_MPU6050/Core/Src/main.c
+++ Temp, gyro mm opsætning/STM32_MPU6050/Core/Src/main.c
@@ -23,6 +23,12 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
+#include "string.h"
+#include "i2c-lcd.h"
+#include "stdio.h"
+
+
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -41,16 +47,15 @@
 
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
+I2C_HandleTypeDef hi2c2;
 
 /* USER CODE BEGIN PV */
 
-static const uint8_t MPU_Address = 0x68 << 1; //Pin AD0 sættes til GND.
-//Da I2C bruger 7 bit addr. og der skal bruges en bit til R/W bit shifter vi 1 til venstre
+static const uint8_t MPU_Address = 0x68;
+
 
 // MPU registrer til temp
-static const uint8_t MPU_TempRegLSB = 0x41 << 1;
-static const uint8_t MPU_TempRegMSB = 0x42 << 1;
-static const uint8_t configRegister = 0x1A << 1;
+static const uint8_t MPU_TempRegLSB = 0x41;
 
 
 
@@ -60,6 +65,7 @@ static const uint8_t configRegister = 0x1A << 1;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
+static void MX_I2C2_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -77,11 +83,14 @@ int main(void)
 {
   /* USER CODE BEGIN 1 */
 
+
 	// En enumeration der beskriver en evt. fejl
-	HAL_StatusTypeDef returnValue = HAL_ERROR;
+	HAL_StatusTypeDef returnValue;
 	uint8_t tempBuf[16];
+
 	int16_t rawTempVal;
-	float tempVal;
+	float tempVal0 = 0.0;
+	float tempVal = 0.0;
 
 
   /* USER CODE END 1 */
@@ -105,11 +114,12 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_I2C1_Init();
+  MX_I2C2_Init();
   /* USER CODE BEGIN 2 */
-  tempBuf[0] = configRegister;
-  tempBuf[1] = 0b00001000;
 
-  HAL_I2C_Master_Transmit(&hi2c1, MPU_Address, tempBuf, 1, HAL_MAX_DELAY);
+  lcd_init();
+
+  lcd_send_string("Hello World");
 
   /* USER CODE END 2 */
 
@@ -117,36 +127,28 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
-	  tempBuf[0] = MPU_TempRegMSB;
-	  tempBuf[1] = MPU_TempRegLSB;
 
-	  returnValue = HAL_I2C_Master_Transmit(&hi2c1, MPU_Address, tempBuf, 1, HAL_MAX_DELAY);
+	  tempBuf[0] = 0x6B;
+	  tempBuf[1] = 0x00;
+	  returnValue = HAL_I2C_Master_Transmit(&hi2c1, (MPU_Address<<1), tempBuf, 2, HAL_MAX_DELAY);
 
-	  if (returnValue != HAL_OK){
-		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
-		  HAL_Delay(2000);
-		  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
-	  }else{
-		  returnValue = HAL_I2C_Master_Receive(&hi2c1, MPU_Address, tempBuf, 2, HAL_MAX_DELAY);
-	  }
+	  tempBuf[0] = 0x41;
+	  returnValue = HAL_I2C_Master_Transmit(&hi2c1, (MPU_Address<<1), tempBuf, 1, HAL_MAX_DELAY);
+	  returnValue = HAL_I2C_Master_Receive(&hi2c1, (MPU_Address<<1) | 0x01, tempBuf, 2, HAL_MAX_DELAY);
 
-	  if (returnValue != HAL_OK){
 
-		  for(int i = 0 ; i < 5 ; i = i + 1){
-			  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
-			  HAL_Delay(500);
-			  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
-			  HAL_Delay(500);
-		  }
 
-	  }else {
+	  // sammensætning af rå data
+	  rawTempVal = ((int16_t)tempBuf[0] << 8 | tempBuf[1]);
+	  tempVal0 = (rawTempVal)/340.0;
+	  tempVal = tempVal0 + 36.53;
 
-		  // sammensætning af rå data
-		  rawTempVal = ((int16_t)tempBuf[0] << 8 | tempBuf[1]);
-		  tempVal = (rawTempVal)/340 + 36.53;
-		  rawTempVal = ((int16_t)tempBuf[0] << 8 | tempBuf[1]);
-	  }
+	  char buf[20];
+	  sprintf(buf, "%f", tempVal);
+
+	  lcd_put_cur(0, 0);
+	  lcd_send_string(buf);
+	  HAL_Delay(200);
 
 
     /* USER CODE END WHILE */
@@ -168,10 +170,13 @@ void SystemClock_Config(void)
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -180,12 +185,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
@@ -226,28 +231,51 @@ static void MX_I2C1_Init(void)
 }
 
 /**
+  * @brief I2C2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C2_Init(void)
+{
+
+  /* USER CODE BEGIN I2C2_Init 0 */
+
+  /* USER CODE END I2C2_Init 0 */
+
+  /* USER CODE BEGIN I2C2_Init 1 */
+
+  /* USER CODE END I2C2_Init 1 */
+  hi2c2.Instance = I2C2;
+  hi2c2.Init.ClockSpeed = 100000;
+  hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c2.Init.OwnAddress1 = 0;
+  hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c2.Init.OwnAddress2 = 0;
+  hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C2_Init 2 */
+
+  /* USER CODE END I2C2_Init 2 */
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
   */
 static void MX_GPIO_Init(void)
 {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : PC13 */
-  GPIO_InitStruct.Pin = GPIO_PIN_13;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
 }
 

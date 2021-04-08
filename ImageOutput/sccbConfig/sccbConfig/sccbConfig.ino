0355e15117942f46592b66a3e2409e03f4fdbd15
Munkeren <rasmus.munksoe@live.dk>
2021-04-08 15:51:33 +0200

    updatedSCCBConfig

--- ImageOutput/sccbConfig/sccbConfig/sccbConfig.ino
+++ ImageOutput/sccbConfig/sccbConfig/sccbConfig.ino
@@ -4,24 +4,43 @@ int address = 42;
 int reg = 61;
 int value = 0b11000000;
 
-*/
+*/void configReg(int address, int setting){
+Wire.beginTransmission(0x21); //Address of camera.
+Wire.write(address);  // send desired register
+Wire.write(setting);  // send desired bit setup
+Wire.endTransmission();
+delay(50);
+}
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
 Wire.begin();
-Wire.beginTransmission(0x21);
-Wire.write(0x3D);  // software reset
-Wire.write(0xC0);
-Wire.endTransmission();
-delay(500); // wait for reset to complete
-Wire.beginTransmission(0x21);
-Wire.write(0x1E);  // software reset
-Wire.write(0x8);
-Wire.endTransmission();
-delay(500);
+configReg(0x12, 0x80); //Software reset
+configReg(0x1E, 0x8);  //Flip image vertically
+configReg(0x13, 0x07); //Auto gain enable, Auto exposure enable, White balance enable
+configReg(0x3f, 0x1f); //Edge enhancement factor 
+configReg(0x43, 0x0a); //Random config to improve white balance
+configReg(0x45, 0x34);
+configReg(0x47, 0x28);
+configReg(0x59, 0x88);
+configReg(0x5b, 0x44);
+configReg(0x5d, 0x49);
+configReg(0x6c, 0x0a);
+configReg(0x6e, 0x11);
+configReg(0x43, 0x0a);
+configReg(0x6a, 0x40);
+configReg(0x44, 0xf0);
+configReg(0x46, 0x58);
+configReg(0x48, 0x3a);
+configReg(0x5a, 0x88);
+configReg(0x5c, 0x67);
+configReg(0x5e, 0x0e);
+configReg(0x6d, 0x55);
+configReg(0x6f, 0x9e);
 
+//Receive one transmission.
 Wire.beginTransmission(0x21); 
-Wire.write(0x1E);  // u now read from reg 0x01
+Wire.write(0x3f);  // u now read from reg 0x01
 Wire.endTransmission();
 Wire.requestFrom(0x21, 1);
 while (Wire.available() == 0);  //block till u get something

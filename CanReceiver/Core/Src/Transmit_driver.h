22968759b80d339e71c2826b0101bb8c8fc0f841
Sophus Graarup <80071781+sgraarup@users.noreply.github.com>
2021-04-24 11:11:49 +0200

    den sorte fredag
    kasper bedte om upload lørdag dagen efter.

---
+++ CanReceiver/Core/Src/Transmit_driver.h
@@ -0,0 +1,30 @@
+/*
+ * Transmit_driver.h
+ *
+ *  Created on: Apr 14, 2021
+ *      Author: micha
+ */
+
+#ifndef SRC_TRANSMIT_DRIVERH
+#define SRC_TRANSMIT_DRIVERH
+
+#include "stm32l4xx_hal.h"
+#include "string.h"
+#include "stdio.h"
+
+
+
+
+void sendGPS(CAN_HandleTypeDef *handler, CAN_TxHeaderTypeDef *transmitHeader);
+
+void sendMCU(CAN_HandleTypeDef *handler, CAN_TxHeaderTypeDef *transmitHeader);
+
+void sendDCMotor(CAN_HandleTypeDef *handler, CAN_TxHeaderTypeDef *transmitHeader);
+
+int messageSplitter(uint8_t *sourceArray, uint8_t *destinationArray, uint8_t position);
+
+void sendData(CAN_HandleTypeDef *handler, uint32_t TxID, uint16_t numOfBytes, uint8_t *dataArray, CAN_TxHeaderTypeDef *transmitHeader);
+
+void floatTo4UIntArray(float floatData, uint8_t destinationArray);
+
+#endif / SRC_TRANSMIT_DRIVERH */
